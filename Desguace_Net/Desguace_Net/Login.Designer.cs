c06407904bd7e91752135f1e9afdd52bdb3de58b
franbn14 <franbn14@gmail.com>
2013-11-25 16:56:11 +0100

    Aplicacion desguace net

---
+++ Desguace_Net/Desguace_Net/Login.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Desguace_Net
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(136, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CIF";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(23, 114);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(180, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(197, 222);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(158, 13);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Si no estas registrada. Hazlo ya!";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 262);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Name = "Login";
+            this.Text = "Desguace Solution";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Sor_Servicios/src/java/CAD/UserCAD.java
@@ -0,0 +1,112 @@
+package CAD;
+
+import CEN.UserCEN;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author esteve
+ */
+
+public class UserCAD{    
+    protected static int create (String name, String pass, String address){
+        int id = -1;
+        String query = "INSERT INTO Usuario (nombre, password, direccion) VALUES ('"+ name +"', '" + pass + "', '" + address + "')";
+        try {
+            id = Connector.updates(query);
+        }
+        catch (ClassNotFoundException | SQLException e){
+            System.err.println(e.getMessage());
+        }
+        return id;
+    }
+    
+    public static void delete(int id){
+        try {
+            String query = "DELETE FROM Usuario WHERE id = " + id;
+            Connector.updates(query);
+        }
+        catch (ClassNotFoundException | SQLException e){
+            System.err.println(e.getMessage());
+        }
+    }
+    
+    public static Hashtable getByID(int id){
+        Hashtable values = new Hashtable();
+        try {
+            String query = "SELECT * FROM Usuario WHERE id = " + id;
+            ResultSet rs = Connector.query(query);
+            
+            if(rs.next()){
+                values.put("id", rs.getString("id"));
+                values.put("name", rs.getString("nombre"));
+                values.put("password", rs.getString("password"));
+                values.put("address", rs.getString("direccion"));
+            }
+            Connector.close(rs);
+        }
+        catch (ClassNotFoundException | SQLException e){
+            System.err.println(e.getMessage());
+        }
+        return values;
+    }
+    
+    public static ArrayList<Hashtable> getAll(){
+        ArrayList<Hashtable> values = new ArrayList();
+        try {
+            String query = "SELECT * FROM Usuario";
+            ResultSet rs = Connector.query(query);
+            while(rs.next()){
+                Hashtable ht = new Hashtable();
+                ht.put("id", rs.getInt("id"));
+                ht.put("name", rs.getString("nombre"));
+                ht.put("password", rs.getString("password"));
+                ht.put("address", rs.getString("direccion"));
+                values.add(ht);
+            }
+            Connector.close(rs);
+        }
+        catch (ClassNotFoundException | SQLException e){
+            System.err.println(e.getMessage());
+        }
+        return values;
+    }
+
+    public static void updateName(int id, String name) {
+        try {
+            String query = "UPDATE Usuario SET nombre = '" + name + "' WHERE id = " + id;
+            Connector.updates(query);
+        }
+        catch (ClassNotFoundException | SQLException e){
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public static void updatePassword(int id, String password) {
+        try {
+            String query = "UPDATE Usuario SET password = '" + password + "' WHERE id = " + id;
+            Connector.updates(query);
+        }
+        catch (ClassNotFoundException | SQLException e){
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public static void updateAddress(int id, String address) {
+        try {
+            String query = "UPDATE Usuario SET direccion = '" + address + "' WHERE id = " + id;
+            Connector.updates(query);
+        }
+        catch (ClassNotFoundException | SQLException e){
+            System.err.println(e.getMessage());
+        }
+    }
+}
