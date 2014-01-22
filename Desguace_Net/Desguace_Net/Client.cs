855ba1083568583a6be5a9b56ac1cee32f171112
franbn14 <franbn14@gmail.com>
2014-01-22 18:04:48 +0100

    ok

---
+++ Desguace_Net/Desguace_Net/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desguace_Net
+{
+    class Client
+    {
+        public string surname;
+        public string nif;
+        public string DOB;
+        public int id;
+        public string name;
+        public string password;
+        public string address;
+    }
+}
---
+++ Desguace_Net/Desguace_Net/Desguace.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desguace_Net
+{
+    class Desguace
+    {
+        public string surname;
+        public string nif;
+        public string deadline;
+        public int code;
+        public int sizeUnit;
+        public string type;
+        public string password;
+        public string address;
+    }
+}
