da7b41e8c7cb7e11fbc3b2e45a2fac284441dfac
alberto <w.whitman22@gmail.com>
2014-03-28 10:50:52 +0000

    Clases SorLogger y ClientLogger

---
+++ SorLogger/src/logger/ClientLogger.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package logger;
+
+import java.util.logging.SimpleFormatter;
+
+/**
+ *
+ * @author alberto
+ */
+public class ClientLogger extends SorLogger {
+    private static String message;
+              
+    public ClientLogger() {
+        super("Log Cliente","logCliente.log");
+    }
+                 
+    public void setLogMessage(int type, String client, String extraInfo) {                
+        message="";
+        
+        switch(type) {
+            case 1: message="Sesión iniciada por "+client;
+                break;
+            
+            case 2: message="Creada solicitud "+extraInfo+" por "+client;
+                break;
+               
+            case 3: message="Eliminada solicitud "+extraInfo+" por "+client;
+                break;
+                
+            case 4: message="Sesión finalizada por "+client;
+                break;
+                
+            case -1: message="Error al iniciar sesión: "+extraInfo+" "+client;
+                break;
+        }        
+        writeLog(message,type);
+    }
+}
---
+++ SorLogger/src/logger/SorLogger.java
@@ -0,0 +1,53 @@
+package logger;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alberto
+ */
+public abstract class SorLogger {
+
+    protected static Logger logger;
+    protected static FileHandler handler;
+    private String type;
+    private String fileName;
+    
+    public Logger getLogger() {
+        return logger;
+    }
+    
+    public SorLogger(String type, String fileName) {
+        try {
+            logger=Logger.getLogger(type,null);
+            logger.setUseParentHandlers(false);
+            handler=new FileHandler("log/"+fileName, true);
+            
+            SimpleFormatter formatter = new SimpleFormatter();  
+            handler.setFormatter(formatter);
+            logger.addHandler(handler);
+            System.out.println("Bien: "+fileName);
+        } catch (IOException ex) {
+            Logger.getLogger(SorLogger.class.getName()).log(Level.SEVERE, null, ex);            
+        } catch (SecurityException ex) {
+            Logger.getLogger(SorLogger.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void writeLog(String message, int type) {                                                      
+        if(type>0)
+            logger.info(message);                
+        else
+            logger.severe(message);                
+    }    
+}
