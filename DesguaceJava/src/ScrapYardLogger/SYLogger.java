0c5bfca6f9e781db21af846a0310b3c619dce7b5
Gustavo <gusty_12pe@hotmail.com>
2014-04-04 20:17:56 +0200

    Añadido el gestor de logs y borrados paquetes innecesarios

---
+++ DesguaceJava/src/ScrapYardLogger/SYLogger.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ScrapYardLogger;
+import java.util.logging.SimpleFormatter;
+/**
+ *
+ * @author Gustavo
+ */
+public class SYLogger extends SorLogger {
+    private static String message;
+              
+    public SYLogger() {
+        super("Log Cliente","logCliente.log");
+    }
+                 
+    public void setLogMessage(int type, String client, String extraInfo) {
+        message="";
+        
+        switch(type) {
+            case 1: message="Sesión iniciada por "+client;
+                break;
+            
+            case 2: message="Creada peticion "+extraInfo+" por "+client;
+                break;
+               
+            case 3: message="Registro completado por "+client;
+                break;
+                
+            case 4: message="Sesión finalizada por "+client;
+                break;
+                
+            case -1: message="Error al iniciar sesión: "+extraInfo+" "+client;
+                break;
+            
+            case -2: message="Error. El usuario ya existe: "+extraInfo+" "+client;
+                break;
+                
+            case -3: message="Error. La peticion no se ha podido realizar: "+extraInfo+" "+client;
+                break;
+        }
+        writeLog(message,type);
+    }
+}
